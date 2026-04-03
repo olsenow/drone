bbe6b7b161bfc43b3227b0cb6cdf6be606b9c3a9
olsen4263 <olsenow4263@outlook.com>
2026-04-03 17:13:00 +0800

    0.1 - viewport tweaks for web and mobile, added battery

--- front-end/src/Soar.jsx
+++ front-end/src/Soar.jsx
@@ -1,5 +1,4 @@
 import {React, useState} from "react";
-
 import './index.css'
 
 export default function Soar() {
@@ -34,29 +33,23 @@ export default function Soar() {
             
 
             
-                <div className="flex flex-col items-center justify-center p-3">
-                    <div className="min-h-screen">
-                        <div className="justify-center items-center mt-3">
-                            Status: <input type="text" placeholder="Disconnected" className="bg-gray-300 border border-gray-500 rounded 
+                <div className="flex items-center justify-center gap-10 p-5">
+                    <div className="justify-center items-center ">
+                        Status : <input type="text" placeholder="Disconnected" className="bg-gray-300 border border-gray-500 rounded 
                                     py-1 px-4 focus:outline-none pointer-events-none text-center " readOnly/>
-                            <div className="border border-gray-500 rounded py-1 px-4 focus:outline-none pointer-events-none text-center">
-                                Battery: 
-                            </div>
-                        </div>
-                        
-
-                        <div id="live-feed" className="border-6 border-gray-400 rounded-lg p-4 mt-4 w-128 h-64 bg-gray-300 flex items-center justify-center">
-                            <div>
-                                <input type="text" placeholder="Drone Live View display" className="text-white-300 pointer-events-none" readOnly/>
-                            </div>
-                            <div className="grid grid-cols-1 z-20" >
-
-                            </div>
-                        </div>
-
-                        
                     </div>
+                    <div className="items-center flex" >
+                        Battery : <progress value="" max="100" className="border border-white rounded ml-5 w-24 h-7"/>
+                    </div>    
                 </div>
+                <div className="flex flex-col items-center justify-center -mt-4">
+                    <div id="live-feed" className="flex flex-col items-center justify-center border-6 border-gray-400 rounded-lg p-4 mt-4 w-128 h-64 bg-gray-300">
+                        <div>
+                            <input type="text" placeholder="Drone Live View display" className="text-white-300 pointer-events-none" readOnly/>
+                        </div>   
+                </div>
+                </div>
+               
           
             </div>
 
